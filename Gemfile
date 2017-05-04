03fce3fd4d7d6d484ad1e0f5a5fab99a51617bc1
saddaqayyum57 <saddaqayyum@gmail.com>
2017-05-05 00:54:11 +0500

    admin portal initial commit

--- Gemfile
+++ Gemfile
@@ -37,6 +37,12 @@ gem 'jbuilder', '~> 2.5'
 gem 'rack-cors', :require => 'rack/cors'
 gem 'devise'
 gem 'simple_token_authentication', '~> 1.0'
+gem 'simple_form'
+
+
+gem 'bootstrap-sass', '~> 3.2.0'
+gem 'autoprefixer-rails'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :v1, defaults: {format: :json} do
     delete '/sessions', :to => 'sessions#destroy'  
@@ -6,4 +7,6 @@ Rails.application.routes.draw do
     resources :registrations, only: [:create]
     resources :pages, only: [:index]
   end
+
+  root :to => "admins#create_course"
 end
