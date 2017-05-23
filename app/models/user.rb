845d5ef23611fac28d71d1184b0a661fcaa9ee5d
saddaqayyum57 <saddaqayyum@gmail.com>
2017-05-23 10:24:51 +0500

    nil name crash fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   def name
-      first_name + " " + last_name
+      first_name.to_s + " " + last_name.to_s
   end
 end
