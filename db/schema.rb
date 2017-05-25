c3402bfd5a9387893ba4db5e916952f43fe0213c
Genius Programmer <me.genius.programmer@gmail.com>
2017-05-25 23:15:57 +0500

    coverted lat lng to decimal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170521181305) do
+ActiveRecord::Schema.define(version: 20170525180018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,8 +79,8 @@ ActiveRecord::Schema.define(version: 20170521181305) do
   create_table "locations", force: :cascade do |t|
     t.string   "town"
     t.string   "state"
-    t.integer  "lat"
-    t.integer  "lng"
+    t.float    "lat"
+    t.float    "lng"
     t.integer  "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
