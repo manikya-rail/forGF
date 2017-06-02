1a7a77288368c123cbd67abf3887fd0189d35b7f
Gabriel <gabriel.bidula@gmail.com>
2017-06-02 13:36:55 -0300

    adjust courses edit

--- app/controllers/admin/courses_controller.rb
+++ app/controllers/admin/courses_controller.rb
@@ -49,7 +49,7 @@ class Admin::CoursesController < ApplicationController
   def update
     respond_to do |format|
       if @course.update(course_params)
-        format.html { redirect_to @course, notice: 'Course was successfully updated.' }
+        format.html { redirect_to admin_courses_path, notice: 'Course was successfully updated.' }
         format.json { render :show, status: :ok, location: @course }
       else
         format.html { render :edit }
