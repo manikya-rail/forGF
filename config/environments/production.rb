58303947f32b25d473f77098e98e33a97bcc8102
britton222 <faisalwaleed@MacBook-Pro-3.local>
2017-07-08 00:51:28 +0500

    fix s3 upload

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
       storage: :s3,
-      bucket: 'ENV['S3_BUCKET_NAME']',
+      bucket: ENV['S3_BUCKET_NAME'],
       s3_credentials: {
         access_key_id: ENV['AWS_ACCESS_KEY_ID'],
         secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
