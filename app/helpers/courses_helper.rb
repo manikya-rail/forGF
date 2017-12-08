f0b8ea41032089c8d558260bdedc0102f201a1dc
Vkhandelwal <vkhandelwal@gmail.com>
2017-12-08 13:52:07 +0530

    fixed chars error

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,10 +1,12 @@
 module CoursesHelper
   def color_selector(color)
-    if color.chars.first != '#' 
-    	color.prepend('#')
-    	return color
-    elsif (color.chars.first == '#' && (color.length == 7 || color.length == 4 ))
-      return color
+    if color.present?
+      if color.chars.first != '#'
+      	color.prepend('#')
+      	return color
+      elsif (color.chars.first == '#' && (color.length == 7 || color.length == 4 ))
+        return color
+      end
     end
   end
 end
