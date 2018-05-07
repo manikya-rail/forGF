06cb9b0e3cd4ffd781776c1a3f1b71194f687cba
vkstpl <vkhandelwal@gmail.com>
2018-05-07 13:25:32 +0530

    Changes for HOMEPAGE COMMERCIAL and TOP BAR / BOTTOM BAR APPEARING & DISAPPEARING

--- app/controllers/embed/pages_controller.rb
+++ app/controllers/embed/pages_controller.rb
@@ -39,33 +39,28 @@ class Embed::PagesController < ApplicationController
   end
 
   def display
-    gon.hole_num = []
-    gon.videos = []
     gon.videos_urls = []
-    gon.tags = []
-    gon.par = []
-    gon.yard = []
-    gon.mhcp = []
-    gon.image = []
     gon.image_urls = []
-    gon.description = []
 
     @course = Course.find(params[:id])
 
     @holes = @course.holes.sort_by{ |m| m.hole_num }
 
-    resolution = (is_mobile? ? 'mobile' : 'medium').to_sym
-    @holes.each do |hole|
-      gon.hole_num << hole.hole_num if hole.video.present?
-      gon.videos << hole.video  if hole.video.present?
-      gon.videos_urls << hole.video.video.url(resolution).gsub('s3-us-west-2.amazonaws.com/fore92', 'd1s5na5d5z3eyp.cloudfront.net') if hole.video.present?
-      gon.tags << hole.video.tags  if hole.video.present?
-      gon.par << hole.par if hole.video.present?
-      gon.yard << hole.yards if hole.video.present?
-      gon.mhcp << hole.mhcp if hole.video.present?
-      gon.image << hole.image_file_name if hole.video.present?
-      gon.image_urls << hole.image.url.gsub('s3-us-west-2.amazonaws.com/fore92', 'd1s5na5d5z3eyp.cloudfront.net') if hole.video.present?
+    @par = []
+    @yards = []
+    @mhcp = []
+    @teename = []
+    @course.score_cards.each do |scorecard|
+      @teename << [scorecard.tee_name, scorecard.id, scorecard.color]
     end
+    @course.holes.each do |hole|
+      @par << hole.par.to_i
+      @yards << hole.yards.to_i
+      @mhcp << hole.mhcp.to_i
+    end
+    resolution = (is_mobile? ? 'mobile' : 'medium').to_sym
+    gon.videos_urls << @course.video.url(resolution).gsub('s3-us-west-2.amazonaws.com/fore92', 'd1s5na5d5z3eyp.cloudfront.net') if @course.video.present?
+    gon.image_urls <<  @course.cover.url.gsub('s3-us-west-2.amazonaws.com/fore92', 'd1s5na5d5z3eyp.cloudfront.net') if @course.cover.present?
   end
 
   def hole_by_hole
@@ -86,6 +81,7 @@ class Embed::PagesController < ApplicationController
     @holes = @course.holes.sort_by{ |m| m.hole_num }
 
     resolution = (is_mobile? ? 'mobile' : 'medium').to_sym
+    @resolution = resolution
     @holes.each do |hole|
       gon.hole_num << hole.hole_num if hole.video.present?
       gon.videos << hole.video  if hole.video.present?
@@ -100,19 +96,25 @@ class Embed::PagesController < ApplicationController
   end
 
   def course_home
+    gon.videos_urls = []
+    gon.image_urls = []
     @course = Course.find(params[:id])
+    @holes = @course.holes.sort_by{ |m| m.hole_num }
     @par = []
     @yards = []
     @mhcp = []
     @teename = []
     @course.score_cards.each do |scorecard|
-      @teename << [scorecard.tee_name, scorecard.id, scorecard.color] 
+      @teename << [scorecard.tee_name, scorecard.id, scorecard.color]
     end
     @course.holes.each do |hole|
       @par << hole.par.to_i
       @yards << hole.yards.to_i
       @mhcp << hole.mhcp.to_i
     end
+    resolution = (is_mobile? ? 'mobile' : 'medium').to_sym
+    gon.videos_urls << @course.video.url(resolution).gsub('s3-us-west-2.amazonaws.com/fore92', 'd1s5na5d5z3eyp.cloudfront.net') if @course.video.present?
+    gon.image_urls <<  @course.cover.url.gsub('s3-us-west-2.amazonaws.com/fore92', 'd1s5na5d5z3eyp.cloudfront.net') if @course.cover.present?
   end
 
   def update_tee_scorecard
