ec78687efd3d7a05ce84068d6a16f878f1b25cd5
vkstpl <vkhandelwal@gmail.com>
2018-08-01 20:59:00 +0530

    Increased memeory limit check for scheduling jobs in sidekiq

--- app/workers/upload_hole_media_worker.rb
+++ app/workers/upload_hole_media_worker.rb
@@ -4,32 +4,23 @@ class UploadHoleMediaWorker
   def perform(hole_params)
     memory_output = %x(free)
     free_memory = memory_output.split(" ")[9]
-    if free_memory.to_i < 30000
+    if free_memory.to_i < 1000000
       UploadHoleMediaWorker.perform_in(10.minutes, hole_params)
       return
     end
   	hole = Hole.find(hole_params["hole_id"])
     video_file = File.open(hole_params["video_file_path"]) rescue nil
-    # cover_file = File.open(hole_params["cover_file_path"]) rescue nil
-    # map_file = File.open(hole_params["map_file_path"]) rescue nil
 
     hole_video = hole.build_video(video: video_file) if video_file.present?
     hole_video.save if hole_video.present?
-    # hole.image = cover_file if cover_file.present?
-    # hole.map = map_file if map_file.present?
-    #hole.description = hole_params["description"]
-    hole.save
     if hole_params["hole_images_paths"].present?
       hole_params["hole_images_paths"].each do |image|
         image_file = File.open(image) rescue nil
         if image_file.present?
           hole.hole_images.create(image: image_file)
-          FileUtils.rm image_file
         end
       end
     end
     FileUtils.rm_rf("public/hole_#{hole_params["hole_id"]}")
-    #FileUtils.rm cover_file if cover_file.present?
-    #FileUtils.rm map_file if map_file.present?
   end
 end
