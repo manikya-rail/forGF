68c7c680a142afe0f5b9e9d51ec49d6d2e2b038d
Giju Ambrose <giju@techversantinfo.com>
2020-05-27 11:39:35 +0530

    Conflict fix in hide carousel and scorecard

--- app/controllers/admin/resorts_controller.rb
+++ app/controllers/admin/resorts_controller.rb
@@ -91,7 +91,7 @@ class Admin::ResortsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def resort_params
-      params.require(:resort).permit(:name, :hide_logo, :hide_about_course, :lead_generation, :logo, :lead_url)
+      params.require(:resort).permit(:name, :hide_logo, :hide_about_course, :lead_generation, :logo, :lead_url, :hide_carousel, :hide_score_card)
     end
 
     def set_version_header
